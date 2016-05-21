66943e9e3aaa87f1347c615161fd0f6fbef2ded8
Paulo Ricardo <rochapaulo@MacBook-Pro-de-Paulo.local>
2016-05-21 06:59:38 -0300

    c

--- mastermind-web/src/main/java/com/pugstunt/mastermind/api/MastermindRS.java
+++ mastermind-web/src/main/java/com/pugstunt/mastermind/api/MastermindRS.java
@@ -1,11 +1,15 @@
 package com.pugstunt.mastermind.api;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -15,6 +19,7 @@ import com.pugstunt.mastermind.core.domain.NewGameRequest;
 import com.pugstunt.mastermind.core.domain.enums.Color;
 import com.pugstunt.mastermind.core.entity.GameEntry;
 import com.pugstunt.mastermind.service.GameService;
+import com.pugstunt.mastermind.service.ImageService;
 import com.pugstunt.mastermind.transformers.GuessTransformer;
 import com.pugstunt.mastermind.transformers.NewGameTransformer;
 
@@ -22,10 +27,10 @@ import com.pugstunt.mastermind.transformers.NewGameTransformer;
 public class MastermindRS {
 	
 	private final GameService gameService;
-	private final GuessImageService guessImageService;
+	private final ImageService guessImageService;
 	
 	@Inject
-	public MastermindRS(final GameService gameService, final GuessImageService guessImageService) {
+	public MastermindRS(final GameService gameService, final ImageService guessImageService) {
 		this.gameService = gameService;
 		this.guessImageService = guessImageService;
 	}
@@ -56,6 +61,13 @@ public class MastermindRS {
 //				.collect(toList());
 		
 		return Response.ok(new GuessTransformer().apply(gameService.checkGuess(gameKey, guess))).build();
+	}
+
+	@GET
+	@Path("/guess_image")
+	@Produces(MediaType.APPLICATION_JSON)
+	public BufferedImage guessImage(@QueryParam("code") String code) throws IOException {
+		return guessImageService.assembleResponseImage(code);
 	}
 
 }
