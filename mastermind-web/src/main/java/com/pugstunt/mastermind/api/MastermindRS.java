27757410f4e3f2d0564286eff0676e5165aec414
Evandro <egzefer@gmail.com>
2016-05-21 03:02:34 -0300

    Guess Image Service

--- mastermind-web/src/main/java/com/pugstunt/mastermind/api/MastermindRS.java
+++ mastermind-web/src/main/java/com/pugstunt/mastermind/api/MastermindRS.java
@@ -1,5 +1,9 @@
 package com.pugstunt.mastermind.api;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -8,9 +12,13 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.pugstunt.mastermind.core.domain.GuessRequest;
+import com.pugstunt.mastermind.service.GuessImageService;
 
 @Path("v1/mastermind")
 public class MastermindRS {
+
+	@Inject
+	private GuessImageService guessImageService;
 
 	@POST
 	@Path("/new_game")
@@ -33,8 +41,8 @@ public class MastermindRS {
 	@GET
 	@Path("/guess_image")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String guessImage() {
-		return "Hello MasterMind - The image for slack use";
+	public BufferedImage guessImage(@QueryParam("code") String code) throws IOException {
+		return guessImageService.assembleResponseImage(code);
 	}
 
 }
