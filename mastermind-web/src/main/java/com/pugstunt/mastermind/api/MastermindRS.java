593d4531e373b1667c545a7452a271d880c3ccf0
Evandro <egzefer@gmail.com>
2016-05-21 01:53:16 -0300

    Creating Rest Service with new_game, guess and guess_image endpoints

---
+++ mastermind-web/src/main/java/com/pugstunt/mastermind/api/MastermindRS.java
@@ -0,0 +1,40 @@
+package com.pugstunt.mastermind.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.pugstunt.mastermind.core.domain.GuessRequest;
+
+@Path("v1/mastermind")
+public class MastermindRS {
+
+	@POST
+	@Path("/new_game")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String newGame(@QueryParam("user") String user) {
+
+		return "Hello MasterMind - New Game";
+	}
+
+	@POST
+	@Path("/guess")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String guess(GuessRequest guessRequest) {
+
+		// TODO Validate keys
+		guessRequest.getCode();
+		return "Hello MasterMind - New Game";
+	}
+
+	@GET
+	@Path("/guess_image")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String guessImage() {
+		return "Hello MasterMind - The image for slack use";
+	}
+
+}
