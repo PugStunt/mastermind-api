634e69a593afe57a7fe2b9dfc3753cfeed992722
Evandro <egzefer@gmail.com>
2016-05-21 05:03:46 -0300

    Color enum changes

--- mastermind-web/src/main/java/com/pugstunt/mastermind/service/GuessImageService.java
+++ mastermind-web/src/main/java/com/pugstunt/mastermind/service/GuessImageService.java
@@ -5,13 +5,14 @@ import static java.util.Objects.isNull;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
-import com.google.common.collect.Lists;
+import javax.imageio.ImageIO;
+
+import com.pugstunt.mastermind.transformers.ColorTransformer;
 
 public class GuessImageService {
 
@@ -19,52 +20,45 @@ public class GuessImageService {
 	private static final int IMG_WIDTH = 50;
 	private static final int OFFSET = 5;
 
-	private static final int[] RED = { 231, 76, 60 };
-	private static final int[] BLUE = { 46, 204, 113 };
-	private static final int[] GREEN = { 52, 152, 219 };
-	private static final int[] YELLOW = { 241, 196, 15 };
-	private static final int[] ORANGE = { 230, 126, 34 };
-	private static final int[] PURPLE = { 155, 89, 182 };
-	private static final int[] CYAN = { 26, 188, 156 };
-	private static final int[] MAGENTA = { 222, 107, 174 };
+	/**
+	 * Receives a simple string with color values and returns an image according
+	 * to the given colors
+	 * 
+	 * @param guessCode
+	 * @return the image that corresponds to the given color parameters
+	 * @throws IOException
+	 */
+	public byte[] assembleResponseImage(String guessCode) throws IOException {
 
-	private static final Map<String, int[]> colorMap = new HashMap<>();
-	static {
-		colorMap.put("R", RED);
-		colorMap.put("B", BLUE);
-		colorMap.put("G", GREEN);
-		colorMap.put("Y", YELLOW);
-		colorMap.put("O", ORANGE);
-		colorMap.put("P", PURPLE);
-		colorMap.put("C", CYAN);
-		colorMap.put("M", MAGENTA);
-	}
-
-	public BufferedImage assembleResponseImage(String guessCode) throws IOException {
-
-		List<String> colors = new ArrayList<>();
-
-		for (int i = 0; i < guessCode.length(); i++) {
-			Character colorCode = guessCode.charAt(i);
-			if (isNull(Lists.newArrayList(com.pugstunt.mastermind.core.domain.enums.Color.values())
-					.contains(colorCode.toString()))) {
-				throw new IllegalArgumentException("Invalid color code: " + colorCode);
-			}
-			colors.add(colorCode.toString());
-		}
+		List<com.pugstunt.mastermind.core.domain.enums.Color> colors = guessCode.chars()
+				.mapToObj(new ColorTransformer()).collect(Collectors.toList());
 
 		BufferedImage img = null;
 
-		for (String color : colors) {
+		for (com.pugstunt.mastermind.core.domain.enums.Color color : colors) {
 			if (isNull(img)) {
-				img = newColor(colorMap.get(color));
+				img = newColor(color.getRgb());
 			} else {
-				img = joinBufferedImages(img, newColor(colorMap.get(color)));
+				img = joinBufferedImages(img, newColor(color.getRgb()));
 			}
 		}
-		return img;
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(img, "jpg", baos);
+		baos.flush();
+		byte[] imageInByte = baos.toByteArray();
+		baos.close();
+		return imageInByte;
 	}
 
+	/**
+	 * Builds a new image that represents a single guess for the given color
+	 * 
+	 * @param rgb
+	 *            an array with the rgb values
+	 * @return a {@link BufferedImage}
+	 * @throws IOException
+	 */
 	private static BufferedImage newColor(int[] rgb) throws IOException {
 
 		BufferedImage img = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, BufferedImage.TYPE_INT_ARGB);
@@ -77,8 +71,11 @@ public class GuessImageService {
 	}
 
 	/**
-	 * join two BufferedImage you can add a orientation parameter to control
-	 * direction you can use a array to join more BufferedImage
+	 * Joins two BufferedImages in one single image
+	 * 
+	 * @param baseImage
+	 * @param joinedImage
+	 * @return a single {@link BufferedImage} upon the merged given images
 	 */
 	public static BufferedImage joinBufferedImages(BufferedImage baseImage, BufferedImage joinedImage) {
 
